b93ce831981b818975fe823206ceaef626052212
Zach Claytor <zclaytor@stsci.edu>
2025-07-30 14:43:09 -0400

    add docstrings

--- profiling/example_script.py
+++ profiling/example_script.py
@@ -1,3 +1,14 @@
+"""
+example_script.py
+
+This script is intended to be called using cProfile in bash. For Example:
+
+>>> python -m cProfile -s tottime example_script.py phast > profiling_phast.txt
+>>> python -m cProfile -s tottime example_script.py t16 /ifs/archive/ops/mast/public/hlsp/t16/s0003/cam1-ccd1 > profiling_t16.txt
+
+There is an executable bash script `profile_mct.sh` in this folder for convienience.
+"""
+
 import os
 from sys import argv
 
@@ -5,6 +16,9 @@ from mast_contributor_tools.filename_check.fc_app import check_filenames, get_fi
 
 
 def run_checker(hlsp_name, directory=None):
+    """
+    If `directory` is not specified, just assume the HLSP's directory on the Test Isilon.
+    """
     if directory is None:
         directory = os.path.join("/ifs/archive/test/mast/public/hlsp", hlsp_name)
 
@@ -25,5 +39,5 @@ if __name__ == "__main__":
     except:
         arg1 = argv[1]
         arg2 = None
-        
+
     run_checker(arg1, arg2)
